e19a849f1f40085b55e1990ce56752922c835915
allamymp <allamympereira@gmail.com>
2023-03-31 09:32:40 -0300

    login updated, homePage/homebutton created

--- script/script.js
+++ script/script.js
@@ -7,15 +7,16 @@ function login(event) {
   event.preventDefault();
 
   const email = document.querySelector('#emailForm').value;
-  const senha = document.querySelector('#passwordForm').value;
+  const password = document.querySelector('#passwordForm').value;
 
   fetch('./DB/registerTest.json')
     .then(response => response.json())
     .then(data => {
-      if (email === data.login && senha === data.senha) {
-        alert('Login realizado com sucesso!');
+      if (email === data.login && password === data.senha) {
+        alert('Login sucessful!');
+      
       } else {
-        alert('Email ou senha incorretos!');
+        alert('Email or password incorretos!');
       }
     })
     .catch(error => console.error(error));
