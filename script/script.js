79e24122466e603222a4cfeceffa19f6b00d6e8c
allamymp <allamympereira@gmail.com>
2023-04-02 06:30:00 -0300

    js register funcion updatetd, script module added

--- script/script.js
+++ script/script.js
@@ -1,86 +1,70 @@
-const form = document.querySelector('form');
-const btnLogin = document.querySelector('#btn-login');
+const fs = require('fs');
 
-btnLogin.addEventListener('click', login);
+if (document.querySelector('#btn-login')) {
+  const form = document.querySelector('form');
+  const btnLogin = document.querySelector('#btn-login');
 
-function login(event) {
-  event.preventDefault();
+  btnLogin.addEventListener('click', login);
 
-  const email = document.querySelector('#emailForm').value;
-  const password = document.querySelector('#passwordForm').value;
+  function login(event) {
+    event.preventDefault();
 
-  fetch('./DB/registerTest.json')
-    .then(response => response.json())
-    .then(data => {
-      if (email === data.login && password === data.senha) {
-        alert('Login sucessful!');
-      
-      } else {
-        alert('Incorrect email or password!');
-      }
-    })
-    .catch(error => console.error(error));
-}
+    const email = document.querySelector('#emailForm').value;
+    const password = document.querySelector('#passwordForm').value;
 
-// Selecting html elements
-const showPasswordButton = document.getElementById('showPassword');
-const passwordInput = document.getElementById('passwordForm');
-const confirmPasswordInput = document.getElementById('confirmPasswordForm');
+    // Lê o arquivo JSON com os usuários
+    const users = JSON.parse(fs.readFileSync('./DB/registerTest.json'));
 
-// Adding a click event to the "Show Password" button
-showPasswordButton.addEventListener('click', () => {
-  // Checking if the button is checked or not
-  if (showPasswordButton.checked) {
-    // If checked, make password entries visible
-    passwordInput.type = 'text';
-    confirmPasswordInput.type = 'text';
-  } else {
-    // Otherwise, hide the entered password
-    passwordInput.type = 'password';
-    confirmPasswordInput.type = 'password';
+    // Busca um usuário pelo email e senha
+    const user = users.find(user => user.email === email && user.password === password);
+
+    if (user) {
+      alert('Login successful!');
+    } else {
+      alert('Incorrect email or password!');
+    }
   }
-});
-
-
-function register(event) {
-event.preventDefault();
-  const firstName = document.getElementById('firstName').value;
-  const lastName = document.getElementById('lastName').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('passwordForm').value;
-  const confirmPassword = document.getElementById('confirmPasswordForm').value;
-  
-  // Cria um objeto de usuário com os valores obtidos
-  const user = {
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    password: password
-  };
-  
-  // Obtém o conteúdo atual do arquivo JSON
-  let users = [];
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", "DB/registerTest.json", false);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      users = JSON.parse(xhr.responseText);
-    }
-  };
-  xhr.send();
-  
-  // Adiciona o novo usuário ao array
-  users.push(user);
-  
-  // Salva o array atualizado no arquivo JSON
-  const xhr2 = new XMLHttpRequest();
-  xhr2.open("POST", "DB/registerTest.json", true);
-  xhr2.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhr2.onreadystatechange = function () {
-    if (xhr2.readyState === 4 && xhr2.status === 200) {
-      console.log("Novo usuário registrado com sucesso!");
-    }
-  };
-  xhr2.send(JSON.stringify(users));
 }
 
+if (document.querySelector('#register-form')) {
+  // Selecting html elements
+  const showPasswordButton = document.getElementById('showPassword');
+  const passwordInput = document.getElementById('passwordForm');
+  const confirmPasswordInput = document.getElementById('confirmPasswordForm');
+
+  // Adding a click event to the "Show Password" button
+  showPasswordButton.addEventListener('click', () => {
+    // Checking if the button is checked or not
+    if (showPasswordButton.checked) {
+      // If checked, make password entries visible
+      passwordInput.type = 'text';
+      confirmPasswordInput.type = 'text';
+    } else {
+      // Otherwise, hide the entered password
+      passwordInput.type = 'password';
+      confirmPasswordInput.type = 'password';
+    }
+  });
+
+  function register(event) {
+    event.preventDefault();
+
+    const formData = {
+      firstName: document.getElementById('firstName').value,
+      lastName: document.getElementById('lastName').value,
+      email: document.getElementById('email').value,
+      password: document.getElementById('passwordForm').value,
+      confirmPassword: document.getElementById('confirmPasswordForm').value
+    };
+
+    // Lê o arquivo JSON com os usuários
+    const users = JSON.parse(fs.readFileSync('./DB/registerTest.json'));
+
+    // Insere um novo usuário no arquivo JSON
+    users.push(formData);
+    fs.writeFileSync('./DB/registerTest.json', JSON.stringify(users));
+
+    console.log(result);
+    // aqui você pode exibir uma mensagem de sucesso para o usuário
+  }
+}
