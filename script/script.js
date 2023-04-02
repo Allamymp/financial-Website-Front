6405a28ed5c1d5b10554899423741952552607af
allamymp <allamympereira@gmail.com>
2023-04-01 22:46:44 -0300

    registerPage.html created

--- script/script.js
+++ script/script.js
@@ -21,3 +21,22 @@ function login(event) {
     })
     .catch(error => console.error(error));
 }
+
+// Selecionando os elementos HTML
+const showPasswordButton = document.getElementById('showPassword');
+const passwordInput = document.getElementById('passwordForm');
+const confirmPasswordInput = document.getElementById('confirmPasswordForm');
+
+// Adicionando um evento de clique no botão "Show Password"
+showPasswordButton.addEventListener('click', () => {
+  // Verificando se o botão está marcado ou não
+  if (showPasswordButton.checked) {
+    // Se estiver marcado, tornar os inputs de senha visíveis
+    passwordInput.type = 'text';
+    confirmPasswordInput.type = 'text';
+  } else {
+    // Caso contrário, ocultar a senha digitada
+    passwordInput.type = 'password';
+    confirmPasswordInput.type = 'password';
+  }
+});
