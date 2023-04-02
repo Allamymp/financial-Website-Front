20e77cbb65b8d24b7c66191e25a20d823c171fcf
allamymp <allamympereira@gmail.com>
2023-04-02 10:56:13 -0300

    register() created

---
+++ script/scriptRegister.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("registerForm");
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      register(event);
+    });
+  });
+  
+  function register(event) {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("passwordForm").value;
+    
+    const user = {
+      name: firstName + " " + lastName,
+      email: email,
+      password: password
+    }
+   
+    alert(`Name: ${user.name}, Email: ${user.email}, Password: ${user.password}`);
+  }
+  
